872739522bbaba6f19676671680b78fd10788e14
masterful-principle-0059 <william.neal83@gmail.com>
2020-09-12 15:33:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,31 +3,36 @@ def game_hash
      :home => {
        :team_name => "Brooklyn Nets",
        :colors => ["Black", "White"],
-       :players => [{
+       :players => [
+         {
          :player_name => "Alan Anderson",
          :number => 0,
          :shoe => 16,
          :points => 22,
          :rebounds => 12,
-         :assists => 12,
+         :assists => 12, 
          :steals => 3,
          :blocks => 1,
-         :slam_dunks => 1},
-         {:player_name => "Reggie Evans",
+         :slam_dunks => 1
+        },
+         {
+           :player_name => "Reggie Evans",
          :number => 30,
          :shoe => 14,
          :points => 12,
          :rebounds => 12,
-         :assists => 12,
+         :assists => 12, 
          :steals => 12,
          :blocks => 12,
-         :slam_dunks => 7},
-         {:player_name => "Brook Lopez",
+         :slam_dunks => 7
+        },
+         {
+           :player_name => "Brook Lopez",
          :number => 11,
          :shoe => 17,
          :points => 17,
          :rebounds => 19,
-         :assists => 10,
+         :assists => 10, 
          :steals => 3,
          :blocks => 1,
          :slam_dunks => 15},
@@ -36,7 +41,7 @@ def game_hash
          :shoe => 19,
          :points => 26,
          :rebounds => 11,
-         :assists => 6,
+         :assists => 6, 
          :steals => 3,
          :blocks => 8,
          :slam_dunks => 5},
@@ -45,40 +50,44 @@ def game_hash
          :shoe => 15,
          :points => 19,
          :rebounds => 2,
-         :assists => 2,
+         :assists => 2, 
          :steals => 4,
          :blocks => 11,
          :slam_dunks => 1}
        ]
-     }
+     },
      :away => {
        :team_name => "Charlotte Hornets",
        :colors => ["Turquoise", "Purple"],
-       :players => [{
+       :players => [
+         {
          :player_name => "Jeff Adrien",
          :number => 4,
          :shoe => 18,
          :points => 10,
          :rebounds => 1,
-         :assists => 1,
+         :assists => 1, 
          :steals => 2,
          :blocks => 7,
          :slam_dunks => 2},
-         {:player_name => "Bismack Biyombo",
+         {
+           :player_name => "Bismack Biyombo",
          :number => 0,
          :shoe => 16,
          :points => 12,
          :rebounds => 4,
-         :assists => 7,
+         :assists => 7, 
          :steals => 22,
          :blocks => 15,
-         :slam_dunks => 10},
-         {:player_name => "DeSagna Diop",
+         :slam_dunks => 10
+        },
+         {
+          :player_name => "DeSagna Diop",
          :number => 2,
          :shoe => 14,
          :points => 24,
          :rebounds => 12,
-         :assists => 12,
+         :assists => 12, 
          :steals => 4,
          :blocks => 5,
          :slam_dunks => 5},
@@ -87,7 +96,7 @@ def game_hash
          :shoe => 15,
          :points => 33,
          :rebounds => 3,
-         :assists => 2,
+         :assists => 2, 
          :steals => 1,
          :blocks => 1,
          :slam_dunks => 0},
@@ -96,12 +105,14 @@ def game_hash
          :shoe => 15,
          :points => 6,
          :rebounds => 12,
-         :assists => 12,
+         :assists => 12, 
          :steals => 7,
          :blocks => 5,
-         :slam_dunks => 12}]
+         :slam_dunks => 12
+         }
+        ] 
      }
    }
- end
- game_hash
+ end 
+ 
  
